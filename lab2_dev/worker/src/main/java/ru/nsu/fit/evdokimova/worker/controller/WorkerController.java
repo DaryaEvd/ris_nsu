4fce1c4edf5c65d3c204c7c39950ea481f98f1f8
DaryaEvd <dhfingerstyle@gmail.com>
2025-04-11 02:23:46 +0700

    added old er of worker

---
+++ lab2_dev/worker/src/main/java/ru/nsu/fit/evdokimova/worker/controller/WorkerController.java
@@ -0,0 +1,25 @@
+package ru.nsu.fit.evdokimova.worker.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.nsu.fit.evdokimova.worker.service.WorkerService;
+import ru.nsu.fit.evdokimova.worker.model.dto.RequestFromManagerToWorker;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/internal/api/worker/hash/crack")
+public class WorkerController {
+
+    private final WorkerService workerService;
+
+    @PostMapping("/task")
+    public ResponseEntity<Void> handleTask(@RequestBody RequestFromManagerToWorker request) {
+        workerService.processTask(request);
+        return ResponseEntity.ok().build();
+    }
+}
