3e1a41968b810ad28bfed0a91bbbdb6acde66fe6
DaryaEvd <dhfingerstyle@gmail.com>
2025-04-22 02:51:21 +0700

    start adding mongodb with its replicas, but doesn't start ok

--- lab2_dev/manager/src/main/java/ru/nsu/fit/evdokimova/manager/listener/ListenerInManager.java
+++ lab2_dev/manager/src/main/java/ru/nsu/fit/evdokimova/manager/listener/ListenerInManager.java
@@ -7,13 +7,13 @@ import ru.nsu.fit.evdokimova.manager.config.RabbitManagerConfig;
 import ru.nsu.fit.evdokimova.manager.model.ResponseToManagerFromWorker;
 import ru.nsu.fit.evdokimova.manager.service.CrackHashManagerService;
 
-@Component
-@RequiredArgsConstructor
-public class ListenerInManager {
-    private final CrackHashManagerService managerService;
-
-    @RabbitListener(queues = RabbitManagerConfig.RESULTS_QUEUE)
-    public void receiveWorkerResponse(ResponseToManagerFromWorker response) {
-        managerService.processWorkerResponse(response);
-    }
-}
+//@Component
+//@RequiredArgsConstructor
+//public class ListenerInManager {
+//    private final CrackHashManagerService managerService;
+//
+//    @RabbitListener(queues = RabbitManagerConfig.RESULTS_QUEUE)
+//    public void receiveWorkerResponse(ResponseToManagerFromWorker response) {
+//        managerService.processWorkerResponse(response);
+//    }
+//}
